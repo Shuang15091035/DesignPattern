46f7cc9194e85c9c79897e32e6b10a8c8cf2ddf0
shuang <1136823976@qq.com>
2018-02-20 20:27:13 +0800

    Initial Commit

---
+++ DesignPattern/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DesignPattern
+//
+//  Created by 刘双 on 2018/2/20.
+//  Copyright © 2018年 llzmh.kongquehui.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ DesignPattern/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DesignPattern
+//
+//  Created by 刘双 on 2018/2/20.
+//  Copyright © 2018年 llzmh.kongquehui.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
